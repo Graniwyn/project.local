f24f54b1ec8f378084cf4d8ab2ebe23665fbf48f
Graniwyn <opifruit@outlook.com>
2022-10-28 12:18:05 +0300

    Done

--- Chapter16/task2.php
+++ Chapter16/task2.php
@@ -1,11 +1,9 @@
 <?php
 $string = "test";
-//$f = fopen("test1.txt", "w+") or die("Ошибка!");
 file_put_contents("test1.txt", $string);
 if (!is_dir($string))
     mkdir($string);
 chdir("test");
 file_put_contents("test2.txt", $string);
-chdir("..");
-chdir("..");
+chdir("../..");
 file_put_contents("test3.txt", $string);

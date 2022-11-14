1e9b2f455d117cc4de9e71b79fa46732308c3cdc
Graniwyn <opifruit@outlook.com>
2022-11-14 10:58:07 +0300

    Done

--- Chapter34/task2.php
+++ Chapter34/task2.php
@@ -1,22 +1,35 @@
 <?php
-if ($_GET['MODE'] === "1") {
-    session_save_path('FIRST_COUNTER');
-    session_start();
-    if (!isset($_SESSION['COUNTER']))
-        $_SESSION['COUNTER'] = 0;
+if (!isset($_GET['MODE']))
+    echo "Ошибка! Передано некорректное значение GET параметра MODE";
+else
+{
+    if ($_GET['MODE'] === "1")
+    {
+        session_save_path('FIRST_COUNTER');
+        session_start();
+        if (!isset($_SESSION['COUNTER']))
+            $_SESSION['COUNTER'] = 0;
+        else
+            $_SESSION['COUNTER']++;
+        echo session_id(), " ", session_name(), " ", session_save_path();
+    }
+    elseif ($_GET['MODE'] === "2")
+    {
+        session_save_path('SECOND_COUNTER');
+        session_start();
+        if (!isset($_SESSION['COUNTER']))
+            $_SESSION['COUNTER'] = 0;
+        else
+            $_SESSION['COUNTER']++;
+        echo session_id(), " ", session_name(), " ", session_save_path();
+    }
     else
-        $_SESSION['COUNTER'] += 1;
-} elseif ($_GET['MODE'] === "2") {
-    session_save_path('SECOND_COUNTER');
-    session_start();
-    if (!isset($_SESSION['COUNTER']))
-        $_SESSION['COUNTER'] = 0;
-    else
-        $_SESSION['COUNTER'] += 1;
-}
-echo session_id(), " ", session_name(), " ", session_save_path();
-if (isset($_GET['DELETE_SESSION_DATA']) and $_GET['DELETE_SESSION_DATA'] === "Y") {
-    $_SESSION = [];
-    unset($_COOKIE[session_name()]);
-    session_destroy();
+        echo "Ошибка! Передано некорректное значение GET параметра MODE";
+
+    if (isset($_GET['DELETE_SESSION_DATA']) and $_GET['DELETE_SESSION_DATA'] === "Y")
+    {
+        $_SESSION = [];
+        unset($_COOKIE[session_name()]);
+        session_destroy();
+    }
 }
